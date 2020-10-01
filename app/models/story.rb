dafc2d1277df73ed0c5efa1543dc3e89f8cf3598
sarahmr63089 <sarahrose63089@gmail.com>
2020-09-30 23:01:18 -0400

    lots of styling

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,7 @@
 class Story < ApplicationRecord
   has_many :scenes, dependent: :destroy
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   def self.published_stories
     stories = Story.all.select { |story| story.published }
